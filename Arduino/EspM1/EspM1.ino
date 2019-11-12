2b06a82bc4a2e5c295448403ea193eb2dc490c51
rodrigolorenzzon <rodrigo.l.basso@pucrs.br>
2019-11-12 16:02:32 -0300

    Atualizando EspM1 para usar o MQTT

--- Arduino/EspM1/EspM1.ino
+++ Arduino/EspM1/EspM1.ino
@@ -1,53 +1,49 @@
 #include <LiquidCrystal.h>
 #include <DHTesp.h>
 #include <Servo.h>
-#include <ESP8266WiFi.h>
 
-void print_dht(float,float);
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
 void gira_motor();
 void gira_motor2();
+void setup_peripherals();
+void setup_wifi();
+void reconnect();
+void callback(char* topic, byte* payload, unsigned int length);
+
 Servo servo;
 int servoPin = 4;
-int pos;
-              //rs, enable, d4, d5, d6, d7
-LiquidCrystal lcd(15, 13, 12, 14, 0, 2);
+
 DHTesp dht;
 int dhtPin = 16;
+              //rs, enable, d4, d5, d6, d7
+LiquidCrystal lcd(15, 13, 12, 14, 0, 2);
 
 char estado = 0; // 0 fechado 1 aberto
 
 // wifi setup
 const char* ssid = "P30_IOT";
 const char* password = "pucrs@2019";
-const char* host = "192.168.30.126";
-const int port = 31600;
+
+// mqtt setup
+const char* mqtt_server = "10.30.152.111";
+const char* mqtt_user = "embarcados";
+const char* mqtt_pass = "embarcados";
+
+// connecting wifi with mqtt
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup()
 {
   Serial.begin(115200);
-  dht.setup(dhtPin, DHTesp::DHT11); // Connect DHT sensor to GPIO 16
-  lcd.begin(16, 2);
-  servo.attach(servoPin);
 
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  Serial.println("Girando motor1");
-  gira_motor();
-
-  Serial.println("Girando motor2");
-  gira_motor2();
+  setup_peripherals();
+  setup_wifi();
   
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop()
@@ -116,6 +112,7 @@ void loop()
 
 void gira_motor()
 {
+  int pos = 0;
   for(pos = 0; pos < 180; pos++){ //PARA "pos" IGUAL A 0, ENQUANTO "pos" MENOR QUE 180, INCREMENTA "pos"
     servo.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
     delay(15); //INTERVALO DE 15 MILISSEGUNDOS
@@ -124,8 +121,68 @@ void gira_motor()
 
 void gira_motor2()
 {
+  int pos = 0;
   for(pos = 180; pos < 0; pos--){ //PARA "pos" IGUAL A 0, ENQUANTO "pos" MENOR QUE 180, INCREMENTA "pos"
     servo.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
     delay(15); //INTERVALO DE 15 MILISSEGUNDOS
   }    
+}
+
+void setup_peripherals()
+{
+  dht.setup(dhtPin, DHTesp::DHT11); // Connect DHT sensor to GPIO 16
+  servo.attach(servoPin);
+  lcd.begin(16, 2);
+}
+
+void setup_wifi()
+{
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("LocalIP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+ // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("EspM1",mqtt_user,mqtt_pass)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("temperatura", "hello world");
+      // ... and resubscribe
+      client.subscribe("umidade");
+    } 
+    else 
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println("");
 }
